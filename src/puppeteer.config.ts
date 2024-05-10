f74279b3f7bd9114c742cb17ac258e95abb9c100
Jovan Suberic <jovan.suberic@fioneer.com>
2024-05-10 23:11:28 +0200

    fix

--- src/puppeteer.config.ts
+++ src/puppeteer.config.ts
@@ -2,7 +2,7 @@ import { join } from 'path';
 import { Configuration } from 'puppeteer';
 
 const puppeteerConfig: Configuration = {
-    cacheDirectory: join('/opt/render/', '.cache', 'puppeteer'),
+    cacheDirectory: join('/opt/render', '.cache', 'puppeteer'),
 };
 
 export default puppeteerConfig;
