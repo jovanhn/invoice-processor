12352c15e5e3814adeacbaeea37c7737697e97c5
Jovan Suberic <jovan.suberic@fioneer.com>
2024-05-12 11:23:08 +0200

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import  { Request, Response }  from 'express';
 const express = require('express');
+const cors = require('cors');
 import {qrExtractor} from "./qr-extractor/qr_extractor";
 import {qrExtractorHtml} from "./qr-extractor/qr-extractor-html";
 const app = express();
@@ -7,9 +8,14 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
+let corsOptions = {
+    origin : ['http://localhost:5173','https://*.github.io'],
+}
+
+app.use(cors(corsOptions))
+
 app.post('/process', (req: Request, res: Response) => {
     const fields = ['shopFullNameLabel','totalAmountLabel','sdcDateTimeLabel', 'addressLabel', 'invoiceNumberLabel']
-
     qrExtractor(req.body.url, fields).then(values => {
         res.json(values);
     }).catch(err => {
