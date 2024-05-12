151b84f1eaf825ae2f3e5477bada7af0397bed90
Jovan Suberic <jovan.suberic@fioneer.com>
2024-05-12 19:43:22 +0200

    add dummy endpoint

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,45 @@ app.get("/", (req: Request, res: Response) => {
     res.json({"message": "QR Code processor!"});
 })
 
+
+
+app.get('/dummy/process',(req: Request, res: Response) => {
+    const r = {
+        "id": "",
+        "totalAmount": "100,00",
+        "dateTime": "26.04.2024. 14:11:34",
+        "shopFullName": "1207084-WOLT",
+        "address": "БУЛЕВАР ВОЈВОДЕ БОЈОВИЋА 8",
+        "invoiceNumber": "9VMEKMSN-DBV8GPO0-3937846",
+        "currency": "RSD",
+        "type": "QR",
+        "items": [
+            {
+                "id": "7d627823-a9ac-45fc-a7ad-b0670a299180",
+                "name": "DELIVERY_ITEM",
+                "amount": "1,00",
+                "priceWithVat": "0,00",
+                "totalPrice": "0,00"
+            },
+            {
+                "id": "f4818370-0e74-44a1-83f7-b344f1cc4b43",
+                "name": "Напојница",
+                "amount": "1,00",
+                "priceWithVat": "100,00",
+                "totalPrice": "100,00"
+            },
+            {
+                "id": "c5fc4892-9ea0-46b3-b7b8-cb1e6ed17a42",
+                "name": "SERVICE_FEE_ITEM",
+                "amount": "1,00",
+                "priceWithVat": "0,00",
+                "totalPrice": "0,00"
+            }
+        ]
+    }
+    res.json(r);
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
